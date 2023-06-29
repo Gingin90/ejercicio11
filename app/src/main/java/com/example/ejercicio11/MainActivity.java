168ad59855d3a40e89c77b1c6d0c0461087de216
ginger cea <gingercea@gmail.com>
2023-06-28 21:31:06 -0400

    ejercicio 11 se ha realizadon los set on click estoy sacando los errores y poniendo los toast

--- app/src/main/java/com/example/ejercicio11/MainActivity.java
+++ app/src/main/java/com/example/ejercicio11/MainActivity.java
@@ -3,25 +3,68 @@ package com.example.ejercicio11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.ejercicio11.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.buttonLevel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+        binding.buttonUnlocked.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        binding.buttonMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Musica en pausa", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.buttonUpdatting.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Cargando perfil ", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        binding.buttonPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "modo galeria", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        binding.buttonPlane.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "modo avion desactivado", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
 }
 
-binding.btn1.setOnClickListener(new View.OnClickListener() {
-@Override
-public void onClick(View view) {
-        Toast.makeText(getBaseContext(), "Boton1 selecionado", Toast.LENGTH_SHORT).show();
-        }
-        });
+
+
+
